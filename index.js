76f3d6363fefd7d439d6c476cc3449767d516fa6
Old <ebhosale73@gmail.com>
2020-10-10 21:31:07 +0530

    first commit

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-var http = require('http');
+const readline = require('readline');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
-
-function read_line(input){
-    // let input=3
+function read_line(input) { //fetch file data
     const readLastLines = require('read-last-lines');
     readLastLines.read("test.txt", input)
         .then((lines) => console.log(lines));
-    }
-    const readline = require('readline');
+}
 
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout
-    });
-    
-    rl.question('Enter number of lines to print ', (input) => {
-      if(input==""){
-          answer=10
-      }
-    read_line(input)
-      rl.close();
-    });
+
+const rl = readline.createInterface({  //take user input
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter number of lines to print ', (input) => { //pass input to function
+    if (input == "") {
+        answer = 10
+    }
+    read_line(input) //call function with user input
+    rl.close();
+});
